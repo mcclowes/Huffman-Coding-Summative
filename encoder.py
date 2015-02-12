c6301d8a5a9dd6c0a46bdbfea8263e0a013a6f4f
Max Clayton Clowes <maxcc@me.com>
2015-02-12 20:03:28 +0000

    Getting somewhere
    Tree construction works, adding in binary construction

--- encoder.py
+++ encoder.py
@@ -17,14 +17,41 @@ def encode_string(inputString):
 	#Copy into a tree
 	tree = charCounter.items()
 	print (tree)
-	#While the tree is bigger than 1
-	#while len(tree) > 1:
-	#Sort
 
-	#take min1 and min2
+	#create binary holder
+	#binaryList = []
+	#for char in tree:
+	#	binaryList.append((char, ''))
+
+	#While the tree is bigger than 1
+	while len(tree) > 1:
+	#Sort (indent)
+		tree.sort(key=lambda tup: tup[1])
+		print (tree)
+	#take min1 and min2 and remove
+		min1 = tuple(tree[0])
+		min2 = tuple(tree[1])
+		print ("Min1 = " + str(min1))
+		print ("Min2 = " + str(min2))
+		del tree[0]
+		del tree[0]
 
 	#combine min1 and min2 into a new tuple, with the summed freq
-	
-	#add in new tuple
+		newTup = ((min1, min2), min1[1] + min2[1])
+		print ("new tup" + str(newTup))
 
-	#add binary 
+	#add in new tuple
+		tree.append(newTup)
+
+	#add binary
+	binaryDict = {}
+	walk_tree()
+
+def walk_tree(node, dic, code):
+	if type(node) = type("STRING"):
+		dic.update({node:code})
+	else:
+		walk_tree(node[0], dic, code+"0")
+		walk_tree(node[1], dic, code+"1")
+	return dic
+		
