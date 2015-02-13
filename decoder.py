189913b6ed2c246cf6f07d734542794d87ec022d
Max Clayton Clowes <maxcc@me.com>
2015-02-13 02:57:52 +0000

    Main method
    Enables multiple test cases

--- decoder.py
+++ decoder.py
@@ -15,12 +15,10 @@ def decode_string(inputString):
 	for i in range(0, len(bitString)):
 		while len(bitString[i])<8:
 			bitString[i] = '0' + bitString[i]
-	#print ('string: ' + str(bitString)) #Correct form
 
 	#Read buffer counter
 	bufferCount = str(bitString[0])
 	bufferCount = int(bufferCount,2)
-	print ("Buffer counter: " + str(bufferCount))
 	#Join into single string + remove buffer digits from end
 	bitString = ''.join(bitString[1:])
 	if bufferCount > 0:
--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from huffman_tools import *
 from datetime import datetime
 
 #Take file input
-filename = 'lorum_ipsum6'
+run('lorum_ipsum')
+run('lorum_ipsum2')
+run('lorum_ipsum3')
+run('lorum_ipsum4')
+run('lorum_ipsum5')
+run('lorum_ipsum6')
 #filename = 'war_and_peace'
 #filename = input('\nInput filename (without file suffix):\n') 
 
-#Encode selected file
-startTime = datetime.now()
-output_encoded_file(filename, encode_string(read_text_file(filename)))
-encodeTime = (datetime.now() - startTime).total_seconds()
+def run(filename):
+	print ('> Running on ' + filename)
+	#Encode selected file
+	startTime = datetime.now()
+	output_encoded_file(filename, encode_string(read_text_file(filename)))
+	encodeTime = (datetime.now() - startTime).total_seconds()
 
-#Read encoded file and output decoded text
-startTime = datetime.now()
-decodedText = read_encoded_file(filename)
-decodeTime = (datetime.now() - startTime).total_seconds()
-print ('\nDecoded text: \n' + str(decodedText))
-output_decoded_file(filename, decodedText)
-print ('Encode time: ' + str(encodeTime) + 's\nDecode time: ' + str(decodeTime) + 's')
+	#Read encoded file and output decoded text
+	startTime = datetime.now()
+	decodedText = read_encoded_file(filename)
+	decodeTime = (datetime.now() - startTime).total_seconds()
+	print ('\nDecoded text: \n' + str(decodedText))
+	output_decoded_file(filename, decodedText)
+	print ('Encode time: ' + str(encodeTime) + 's\nDecode time: ' + str(decodeTime) + 's\n\n')
